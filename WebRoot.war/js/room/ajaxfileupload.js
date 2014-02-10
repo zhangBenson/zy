4bfcdfd36e3d34325d68a79fd06d8a8f15b98234
mytream <422104389@qq.com>
2014-02-10 19:07:25 +0800

    ppt uploading function in teacher virtual room

--- WebRoot.war/js/room/ajaxfileupload.js
+++ WebRoot.war/js/room/ajaxfileupload.js
@@ -186,6 +186,19 @@ jQuery.extend({
             jQuery("<div>").html(data).evalScripts();
 
         return data;
+    },
+
+    handleError: function( s, xhr, status, e ){
+// If a local callback was specified, fire it
+        if ( s.error ) {
+            s.error.call( s.context || s, xhr, status, e );
+        }
+
+        // Fire the global callback
+        if ( s.global ) {
+            (s.context ? jQuery(s.context) : jQuery.event).trigger( "ajaxError", [xhr, s, e] );
+        }
     }
+
 })
 
