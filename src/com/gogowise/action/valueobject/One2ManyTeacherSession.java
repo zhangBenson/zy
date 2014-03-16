3d58a38c07a507002eeb59da46ba5078984930b6
Ji JianHui <jjh2008303447@gmail.com>
2014-03-16 20:44:58 +0800

    change the flash session <content>

--- src/com/gogowise/action/valueobject/One2ManyTeacherSession.java
+++ src/com/gogowise/action/valueobject/One2ManyTeacherSession.java
@@ -29,7 +29,7 @@ public class One2ManyTeacherSession {
         if(courseClass.getCourse().getPersonalTeacher()!=null) this.setType(4);
         this.setTitle(getEmptyString(courseClass.getCourse().getName()));
         this.setAbstract(getEmptyString(""));
-        this.setContent(getEmptyString(courseClass.getCourse().getDescription()));
+        //this.setContent(getEmptyString(courseClass.getCourse().getDescription()));
         this.setClassID(getEmptyInteger(courseClass.getId()));
         this.setCourseID(getEmptyInteger(courseClass.getCourse().getId()));
     }
