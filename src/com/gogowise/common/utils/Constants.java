19531474497ebfa828dc8ce78d68f2b8aedea075
zlhdes <zlhades@outlook.com>
2014-03-18 10:15:09 +0800

    add default config

--- src/com/gogowise/common/utils/Constants.java
+++ src/com/gogowise/common/utils/Constants.java
@@ -49,7 +49,7 @@ public class Constants {
     public static final String TYPE_JUDGE_STRING = "2";
     public static final String TYPE_HOST_STRING = "3";
 
-    public static final String WHICH_SITE_CAN＿HAVE＿SECDOMAIN = "www.gogowise.com";
+    public static final String WHICH_SITE_CAN＿HAVE＿SECDOMAIN = "test.gogowise.com";
     public static final int MIN_SECDOMAIN_LENGTH = 5;
     public static final String DEFAULT_BLANK_VALUE = "";
     public static final String DEFAULT_USER_LOGO = "images/defaultImgs/portrait.png";
