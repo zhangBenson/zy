17b5518fc3252abf3cea518e20b73b345d356d99
zlhdes <zlhades@outlook.com>
2014-06-13 10:34:09 +0800

    add base user role type sql

---
+++ etc/sql/baseuserroletype.sql
@@ -0,0 +1,6 @@
+insert into `baseuserroletype`(`baseUser_id`,`roleType_id`) values (1,1);
+insert into `baseuserroletype`(`baseUser_id`,`roleType_id`) values (1,2);
+insert into `baseuserroletype`(`baseUser_id`,`roleType_id`) values (1,3);
+insert into `baseuserroletype`(`baseUser_id`,`roleType_id`) values (1,4);
+insert into `baseuserroletype`(`baseUser_id`,`roleType_id`) values (1,5);
+insert into `baseuserroletype`(`baseUser_id`,`roleType_id`) values (1,6);
