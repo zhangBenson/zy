b415fa4258cc0c6a760c29b723cdacdc2184cf6f
zlhdes <zlhades@outlook.com>
2014-08-05 08:32:26 +0800

    add config for special.

---
+++ src/com/gogowise/common/config/Configuration.java
@@ -0,0 +1,15 @@
+package com.gogowise.common.config;
+
+
+public class Configuration {
+
+    private String hostName;
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+}
