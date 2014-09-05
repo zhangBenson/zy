f39a0f72946756f956b0a7bba00cd10d8947a055
JiJianHui <jjh2008303447@gmail.com>
2014-09-05 10:57:32 +0800

    revert the index page

---
+++ src/com/gogowise/common/config/ConfigConstants.java
@@ -0,0 +1,12 @@
+package com.gogowise.common.config;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ji JianHui
+ * Time: 2014-09-04 22:14
+ * Email: jhji@ir.hit.edu.cn
+ */
+public class ConfigConstants
+{
+    public static String SITE_NAME =  null;
+}
