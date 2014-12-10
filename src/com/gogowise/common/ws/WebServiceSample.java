95a74da83cf956afafd39e9019cb9e11dcf43d19
zlhdes <zlhades@outlook.com>
2014-12-10 10:55:02 +0800

    remove web service

--- src/com/gogowise/common/ws/WebServiceSample.java
+++ src/com/gogowise/common/ws/WebServiceSample.java
@@ -1,26 +1,26 @@
-package com.gogowise.common.ws;
-
-import javax.jws.WebParam;
-import javax.jws.WebResult;
-import javax.jws.WebService;
-
-
-@WebService
-public interface WebServiceSample {
-
-
-    boolean logon(String email, String password);
-
-    String say(String hello);
-
-
-    String sayUserName(
-            @WebParam(name = "user")
-            UserDTO user);
-
-
-    public
-    @WebResult(partName = "o")
-    ListObject findUsers();
-
-}
+//package com.gogowise.common.ws;
+//
+//import javax.jws.WebParam;
+//import javax.jws.WebResult;
+//import javax.jws.WebService;
+//
+//
+//@WebService
+//public interface WebServiceSample {
+//
+//
+//    boolean logon(String email, String password);
+//
+//    String say(String hello);
+//
+//
+//    String sayUserName(
+//            @WebParam(name = "user")
+//            UserDTO user);
+//
+//
+//    public
+//    @WebResult(partName = "o")
+//    ListObject findUsers();
+//
+//}
